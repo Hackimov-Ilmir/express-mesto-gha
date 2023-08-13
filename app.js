0e711d1e3aa7a27f4fc6e3cf021bf8a5d809c55e
Ilmir Khakimov <khakimoff.ilmir@yandex.ru>
2023-08-13 23:57:34 +0500

    test

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-const User = require('./models/users');
+const router = require('./routes/index');
 
 const app = express();
-
-app.use(bodyParser.json());
 
 const port = 3000;
 
@@ -14,19 +11,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.post('/users', (req, res) => {
-  User.create({ ...req.body })
-    .then((user) => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Server Error');
-    });
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64d4c9e8a68d6a133eafb1f4',
+  };
+
+  next();
 });
 
-app.get('/', (req, res) => {
-  res.send('test123');
-});
+app.use(router);
 
 app.listen(port, console.log(`listen ${port}`));
---
+++ controllers/users.js
@@ -0,0 +1,81 @@
+const User = require('../models/users');
+
+const getUsers = (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+};
+
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        return res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(400)
+          .send({
+            message: 'Переданы некорректные данные при создании пользователя.',
+          });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
+
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        return res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении профиля.',
+        });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении аватара',
+        });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  updateAvatar,
+};
