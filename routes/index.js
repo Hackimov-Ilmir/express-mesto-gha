9014798eeaf8c68038156e54470fd54b6b24ed4a
Ilmir Khakimov <khakimoff.ilmir@yandex.ru>
2023-08-18 20:00:26 +0500

    commit2

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
+const express = require('express');
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const NotFound = require('../errors/NotFound');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Not Found' });
+router.use((req, res, next) => {
+  next(new NotFound('Такая страница не существует'));
 });
+router.use(express.json());
 
 module.exports = router;
