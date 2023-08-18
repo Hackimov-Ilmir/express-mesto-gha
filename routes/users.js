691fb4686af15b6c2a2c8db520b8819a5ee0c6fe
Ilmir Khakimov <khakimoff.ilmir@yandex.ru>
2023-08-18 19:26:46 +0500

    test3

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,16 @@ const {
   getUserInfo,
 } = require('../controllers/users');
 
+const {
+  validationUpdateUser,
+  validationUpdateAvatar,
+  validationUserId,
+} = require('../middlewares/validation');
+
 router.get('/', getUsers);
-router.get('/:userId', getUserById);
+router.get('/:userId', validationUserId, getUserById);
 router.get('/me', getUserInfo);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me', validationUpdateUser, updateUser);
+router.patch('/me/avatar', validationUpdateAvatar, updateAvatar);
 
 module.exports = router;
